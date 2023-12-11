19c4d7229510cfca26bb4f1109b52e532fa11117
Chinmay <chinmaybarik214@gmail.com>
2023-12-11 20:59:10 +0530

    we don’t need the pos[]=i stuff sol is still the same

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-def solve(board):
+def solve(board): # if board is solved returns True otherwise returns False
     # Uncomment below code to see board after each recursion
     #
     # print("\n")
@@ -20,16 +20,16 @@ def solve(board):
     return False # into line 15 | if solve(board): recives False, else block above is run
 def isValid(board, num, pos): # if move is valid returns True otherwise returns False
     for i in range(9): # 9 = no. of columns
-        if board[pos[0]][i] == num and pos[1] != i:
+        if board[pos[0]][i] == num:
             return False
     for i in range(9): # 9 = no. of rows
-        if board[i][pos[1]] == num and pos[0] != i:
+        if board[i][pos[1]] == num:
             return False
     box_x = pos[1] // 3
     box_y = pos[0] // 3
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if board[i][j] == num and (i, j) != pos:
+            if board[i][j] == num:
                 return False
     return True
 def print_board(board):
@@ -50,15 +50,15 @@ def emptyCheck(board):
                 return (i, j) # (row, col)-index of first zero found in sudoku
     return False # if no 0s present in sudoku
 board = [
-[8, 9, 4, 0, 0, 0, 0, 5, 1],
-[0, 0, 7, 0, 0, 3, 0, 6, 9],
-[0, 6, 0, 5, 0, 4, 0, 0, 0],
-[0, 3, 8, 4, 5, 1, 0, 0, 0], 
-[2, 0, 0, 0, 0, 6, 8, 0, 5],
-[6, 0, 0, 0, 0, 2, 7, 0, 0],  
-[3, 8, 0, 1, 7, 5, 0, 0, 0],
-[4, 0, 0, 3, 6, 9, 1, 0, 8],
-[0, 1, 0, 0, 0, 0, 5, 7, 0] 
+[0, 8, 0, 0, 0, 6, 2, 0, 0],
+[5, 0, 0, 8, 7, 0, 3, 0, 0],
+[0, 0, 0, 0, 0, 4, 0, 7, 0],
+[0, 4, 0, 2, 1, 0, 0, 3, 0], 
+[0, 0, 9, 0, 0, 0, 5, 0, 0],
+[0, 0, 0, 0, 0, 7, 0, 0, 0],  
+[0, 0, 0, 6, 0, 0, 0, 0, 0],
+[0, 2, 0, 3, 8, 0, 0, 1, 0],
+[4, 0, 0, 0, 0, 0, 0, 0, 2] 
 ]
 # Uncomment below code to input sudoku from user:
 #
